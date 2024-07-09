a802554d82b0cc97818567e794310be76b09a1e4
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-09 16:06:56 +0900

    레포지토리 생성

---
+++ src/main/java/example/test/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package example.test.repository;
+
+import example.test.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+}
