40fd9382dab457de9069c6d8670fd758c7fb6ff5
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-09 16:12:54 +0900

    컨트롤러 생성

---
+++ src/main/java/example/test/controller/UserController.java
@@ -0,0 +1,19 @@
+package example.test.controller;
+
+import example.test.domain.User;
+import example.test.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/login")
+    public void login(User user) {
+        userService.login(user);
+    }
+}
