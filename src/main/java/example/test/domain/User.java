49f29dbbc81aac122e6a59f4d0712dc8b12f420e
koo995 <107671886+koo995@users.noreply.github.com>
2024-07-09 20:05:52 +0900

    3-커밋2

--- src/main/java/example/test/domain/User.java
+++ src/main/java/example/test/domain/User.java
@@ -1,15 +1,21 @@
 package example.test.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import example.test.Post;
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import java.util.Set;
 
 @Entity
+@NoArgsConstructor
 public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long pk;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Post> posts;
 
     private String name2;
 }
